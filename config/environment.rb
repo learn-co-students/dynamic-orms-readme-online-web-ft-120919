3c47c9ddf7667e7b4517697a8d5e29df81de93f0
Zach Cullen <zacullen@gmail.com>
2020-01-27 13:09:13 -0500

    Done.

--- config/environment.rb
+++ config/environment.rb
@@ -1,4 +1,5 @@
 require 'sqlite3'
+require 'pry'
 
 
 DB = {:conn => SQLite3::Database.new("db/songs.db")}
@@ -13,4 +14,4 @@ sql = <<-SQL
 SQL
 
 DB[:conn].execute(sql)
-DB[:conn].results_as_hash = true
+
